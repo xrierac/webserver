15c4f44a680e352aa193bbb4aae8727db5d34917
xriera-c <149150079+xrierac@users.noreply.github.com>
2024-11-27 14:54:27 +0200

    Working version without body

--- include/HttpParser.hpp
+++ include/HttpParser.hpp
@@ -1,34 +1,43 @@
 #pragma once
+#include <cstddef>
 #include <string_view>
-#include <string>
+#include <unordered_map>
 
-enum http_method {
+#define MAX_REQ_LINE_SIZE 8000
+#define MAX_METHOD_SIZE 6
+
+enum e_http_method {
 	DELETE = 0,
 	GET = 1,
 	POST = 2,
 	UNKNOWN = 3
 };
 
-enum states {
-	s_init,
-	s_get_method,
-	s_get_uri,
-	s_get_http,
-	s_get_headers,
-	s_end
-};
-
 class HttpParser {
 	private:
-		http_method _method;
-		std::string_view _uri;
-		states _state;
-	public:
-		HttpParser();
-		size_t readMethod(const std::string_view data);
-		http_method getMethod() const;
-		void startParsing(std::string data);
-		size_t httpParserExecute();
+		std::string_view								_request;
+		e_http_method									_method;
+		std::string_view								_method_string;
+		std::string_view								_target;
+		bool											_valid_http;
+		std::unordered_map<std::string_view, std::string_view>	_headers;
+		const size_t									_body_size;
+		std::string_view								_body;
+		unsigned int									_status;
 
-		static const std::string methodToString(http_method method);
+		void	checkReqLineErrors(); // to do
+		size_t	parseMethod();
+		size_t	parseTarget(size_t index);
+		size_t	parseHttp(size_t index);
+		size_t	parseHeaders(size_t index);
+		void	parseBody(std::string request);
+	public:
+		HttpParser(size_t size);
+		void 					parseRequest(std::string_view request);
+		e_http_method			getMethod();
+		std::string_view		getMethodString();
+		std::string_view		getTarget();
+		bool 					isValidHttp();
+		std::unordered_map<std::string_view, std::string_view> getHeaders();
+
 };
--- src/HttpParser.cpp
+++ src/HttpParser.cpp
@@ -1,45 +1,97 @@
 #include "HttpParser.hpp"
+#include <stdexcept>
 
-HttpParser::HttpParser() : _state(s_init) {}
+// Constructor
+HttpParser::HttpParser(size_t size) : _body_size(size) {}
 
-http_method HttpParser::getMethod() const {return _method;}
+//Getters
+e_http_method HttpParser::getMethod() { return _method;}
+std::string_view HttpParser::getMethodString() {return _method_string;}
+std::string_view HttpParser::getTarget() { return _target;}
+bool HttpParser::isValidHttp() { return _valid_http;}
+std::unordered_map<std::string_view, std::string_view> HttpParser::getHeaders() { return _headers;}
 
-size_t HttpParser::readMethod(const std::string_view data)
+void	HttpParser::checkReqLineErrors()
 {
-	switch(data[0])
-	{
-		case 'G':
-			_method = GET;
-		case 'D':
-			_method = DELETE;
-		case 'P':
-			_method = POST;
-		default:
-			_method = UNKNOWN;
-	}
-	for(size_t i = 0; i < data.size(); i++)
-	{
-
-	}
 
 }
 
-void HttpParser::startParsing(std::string data)
+size_t HttpParser::parseMethod()
 {
-	std::string_view view_data {data};
-	for(size_t i = 0; i < view_data.size(); i++)
-	{
-
-	}
+	size_t index = _request.find(' ');
+	_method_string = _request.substr(0, index);
+	if (_method_string == "GET")
+		_method = GET;
+	else if (_method_string == "POST")
+		_method = POST;
+	else if (_method_string == "DELETE")
+		_method = DELETE;
+	else
+		_method = UNKNOWN;
+	return index;
 }
 
-const std::string HttpParser::methodToString(http_method method)
+size_t HttpParser::parseTarget(size_t index)
 {
-	switch(method)
+	while (index < _request.size() && std::isspace(_request[index]))
+		index++;
+	size_t new_index = _request.find(' ', index);
+	_target = _request.substr(index, new_index - (index));
+	return new_index;
+}
+
+size_t HttpParser::parseHttp(size_t index)
+{
+	while (index < _request.size() && std::isspace(_request[index]))
+		index++;
+	size_t new_index = _request.find("\r\n", index);
+	std::string_view sv = _request.substr(index, new_index - (index));
+	if (sv == "HTTP/1.1")
+		_valid_http = true;
+	else
+		_valid_http = false;
+	return new_index;
+}
+
+size_t HttpParser::parseHeaders(size_t index)
+{
+	size_t iter, end_line;
+	index += 2;
+	size_t end_headers = _request.find("\r\n\r\n", index);
+	while (index < end_headers)
 	{
-		case GET: return "GET";
-		case POST: return "POST";
-		case DELETE: return "DELETE";
-		default: return "UNKNOWN";
+		iter = index;
+		end_line = _request.find("\r\n", index);
+		while (iter < end_line && _request[iter] != ':')
+			iter++;
+		if (_request[iter - 1] == ' ')
+			throw std::invalid_argument("Wrong headers.");
+		std::string_view key = _request.substr(index, iter - index);
+		iter++;
+		while (iter < end_line && _request[iter] == ' ')
+			iter++;
+		index = iter;
+		while (iter < end_line)
+			iter++;
+		std::string_view value = _request.substr(index, iter - index);
+		_headers.emplace(key, value);
+		index = end_line + 2;
 	}
+	return end_headers;
+}
+
+void HttpParser::parseRequest(std::string_view request)
+{
+	_request = request;
+	size_t index;
+	try {
+		index = parseMethod();
+		if (_method == UNKNOWN)
+			throw std::invalid_argument("Wrong method.");
+		index = parseTarget(index);
+		index = parseHttp(index);
+		if (_valid_http == false)
+			throw std::invalid_argument("Wrong HTTP version.");
+		index = parseHeaders(index);
+	} catch (...) {}
 }
